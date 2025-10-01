968090f09ee1a3609320f910f3b76e8be2887b7e
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2025-10-01 17:30:02 +0200

    Create 1_run_feems.py

---
+++ 3_Population_structure/feems/1_run_feems.py
@@ -0,0 +1,110 @@
+# 1. Import - first import base
+import numpy as np
+# import pkg_resources -> deprecated
+from importlib import resources
+from sklearn.impute import SimpleImputer
+from pandas_plink import read_plink
+import statsmodels.api as sm
+
+# viz
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+# feems
+from feems.utils import prepare_graph_inputs, cov_to_dist
+from feems.objective import comp_mats
+from feems.viz import draw_FEEMSmix_surface, plot_FEEMSmix_summary
+from feems import SpatialGraph, Objective, Viz
+
+# 2. change matplotlib fonts
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["font.sans-serif"] = "Arial"
+
+# 3. Get data path
+
+data_path = str(resources.files('feems') / 'data')
+
+# read the genotype data and mean impute missing data
+(bim, fam, G) = read_plink("{}/wolvesadmix".format(data_path))
+imp = SimpleImputer(missing_values=np.nan, strategy="mean")
+genotypes = imp.fit_transform((np.array(G)).T)
+
+print("n_samples={}, n_snps={}".format(genotypes.shape[0], genotypes.shape[1]))
+
+# 4. setup graph
+coord = np.loadtxt("{}/wolvesadmix.coord".format(data_path))  # sample coordinates
+outer = np.loadtxt("{}/wolvesadmix.outer".format(data_path))  # outer coordinates
+grid_path = "{}/grid_100.shp".format(data_path)  # path to discrete global grid
+
+# graph input files
+outer, edges, grid, _ = prepare_graph_inputs(coord=coord, 
+                                             ggrid=grid_path,
+                                             translated=True, 
+                                             buffer=0,
+                                             outer=outer)
+
+# 5. Exploratory plot
+
+plt.figure(dpi=200, figsize=(8,6))
+projection = ccrs.AzimuthalEquidistant(central_longitude=-100)
+ax = plt.axes(projection=projection)
+
+# add coastlines, borders, and land features
+ax.add_feature(cfeature.COASTLINE, edgecolor='#636363', linewidth=0.5)
+ax.add_feature(cfeature.BORDERS, edgecolor='gray', linewidth=0.3)
+ax.add_feature(cfeature.LAND, facecolor='#f7f7f7')
+
+# add grid points
+ax.scatter(grid[:, 0], grid[:, 1], s=3, color='grey', alpha=0.7, transform=ccrs.PlateCarree(), label='grid point')
+
+# add outer boundary
+ax.plot(outer[:, 0], outer[:, 1], color='black', linewidth=1, transform=ccrs.PlateCarree(), label='outer boundary')
+
+# add edges
+for edge in edges:
+    i, j = edge - 1
+    ax.plot([grid[i, 0], grid[j, 0]], [grid[i, 1], grid[j, 1]], 
+            color='lightgray', linewidth=1, alpha=0.6, transform=ccrs.PlateCarree())
+
+# add sample points
+ax.scatter(coord[:, 0], coord[:, 1], s=8, color='black', zorder=2,transform=ccrs.PlateCarree(), label='sample points')
+plt.legend()
+
+# 6. Prepare the spatial graph
+
+sp_graph = SpatialGraph(genotypes, coord, grid, edges, scale_snps=True)
+projection = ccrs.AzimuthalEquidistant(central_longitude=-100)      # Choose are and projection
+
+# 7. Map of the coordinates and grid
+
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=10, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map(longlat=True)
+v.draw_samples()
+v.draw_edges(use_weights=False)
+v.draw_obs_nodes(use_ids=False)
+
+# 8. Fit feems - but first choose lambda and lambd_q (residual penalization) in the cross validation analysis in script 0_...sh
+
+sp_graph.fit(lamb = 2.0, lamb_q = 10.0, optimize_q = 'n-dim')
+
+# 9. Final plot 
+
+fig = plt.figure(dpi=300)
+ax = fig.add_subplot(1, 1, 1, projection=projection)  
+v = Viz(ax, sp_graph, projection=projection, edge_width=.5, 
+        edge_alpha=1, edge_zorder=100, sample_pt_size=20, 
+        obs_node_size=7.5, sample_pt_color="black", 
+        cbar_font_size=10)
+v.draw_map()
+v.draw_edges(use_weights=True)
+v.draw_obs_nodes(use_ids=False) 
+v.draw_edge_colorbar()
+
+# 10. Visualize model fit
+
